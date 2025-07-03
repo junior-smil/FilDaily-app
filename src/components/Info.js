1b758b0dd68a0340ad81f243e5795e72b15ee4a5
junior-smil <juniorsmil24@gmail.com>
2025-07-03 14:13:31 +0100

    style debut

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Info.css'
 import {  } from "react-icons/ri";
 import { Link } from 'react-router-dom';
 
-function Info() {
-  return (
-    <div className="info">
-      <h1>Informations</h1>
-      <p>Ceci est la page d'informations.</p>      <div>
-       <Link to="/">Retour à l'accueil</Link>
-      </div>
-    </div>
-  );
-}
+
+const articles = [
+    {
+        id: 1,
+        title: 'Article 1',
+        description: 'Description de l\'article 1.',
+        image: 'image1.jpg',
+    },
+    {
+        id: 2,
+        title: 'Article 2',
+        description: 'Description de l\'article 2.',
+        image: 'image2.jpg',
+    },
+    {
+        id: 3,
+        title: 'Article 3',
+        description: 'Description de l\'article 3.',
+        image: 'image3.jpg',
+    },
+];
+
+const Info = () => {
+    const [selectedArticle, setSelectedArticle] = useState(null);
+
+    const handleArticleClick = (article) => {
+        setSelectedArticle(article);
+    };
+
+    return (
+        <div className="journal-container">
+            <div className="details">
+                {selectedArticle ? (
+                    <>
+                        <h2>{selectedArticle.title}</h2>
+                        <img src={selectedArticle.image} alt={selectedArticle.title} />
+                        <p>{selectedArticle.description}</p>
+                    </>
+                ) : (
+                    <p>Sélectionnez un article pour voir les détails ici.</p>
+                )}
+            </div>
+            <div className="sidebar">
+                <h2>Articles</h2>
+                {articles.map((article) => (
+                    <div
+                        key={article.id}
+                        className="article"
+                        onClick={() => handleArticleClick(article)}
+                    >
+                        <h3>{article.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Info;
